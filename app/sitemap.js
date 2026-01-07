8fe1ea94818385ea323ab31e57be07b424edec65
Revin Ramuh <revjai1101@gmail.com>
2026-01-07 11:20:06 +0800

    Added automatic sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  const baseUrl = 'https://www.easytoolilfy.com';
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
